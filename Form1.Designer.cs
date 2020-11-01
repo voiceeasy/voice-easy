fcfaf056746195503b479a31c123b0c5d69cc5b7
nivedsanil <nivedsanil@users.noreply.github.com>
2020-11-01 20:36:05 +0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.Back = new System.Windows.Forms.Button();
+            this.Forward = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(4, 98);
+            this.webBrowser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(30, 31);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1251, 595);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(44, 54);
+            this.Back.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(112, 35);
+            this.Back.TabIndex = 1;
+            this.Back.Text = "Back\r\n";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Forward
+            // 
+            this.Forward.Location = new System.Drawing.Point(165, 54);
+            this.Forward.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Forward.Name = "Forward";
+            this.Forward.Size = new System.Drawing.Size(112, 35);
+            this.Forward.TabIndex = 2;
+            this.Forward.Text = "Forward\r\n";
+            this.Forward.UseVisualStyleBackColor = true;
+            this.Forward.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(286, 54);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Refresh\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(408, 54);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Home\r\n";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1095, 53);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "GO!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(529, 59);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 26);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(44, 9);
+            this.btnEnable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(158, 35);
+            this.btnEnable.TabIndex = 7;
+            this.btnEnable.Text = "voice enable";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(222, 9);
+            this.btnDisable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(176, 35);
+            this.btnDisable.TabIndex = 8;
+            this.btnDisable.Text = "voice disable";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(529, 18);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(554, 26);
+            this.searchBox.TabIndex = 9;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(1095, 13);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(112, 31);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 692);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Forward);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.webBrowser);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Forward;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("www.google.com");
+        }
+
+        private void textBox1_TextChanged(object sender,EventArgs e)
+        {
+            
+            
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer s = new SpeechSynthesizer();
+            Choices list = new Choices();
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+            list.Add(new String[] { "google.com", "yahoo.com","vtop.vit.ac.in","youtube.com" });
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += button5_Click;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+        }
+      
+        private void button5_Click(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+            webBrowser.Navigate(r);
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            webBrowser.Navigate(e.Result.Text);
+            textBox1.Text = e.Result.Text;
+           
+
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(textBox1.Text);
+            btnDisable.Enabled = true;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            this.webBrowser.Navigate("http://www.google.com/search?q=" + searchBox.Text);
+        }
+    }
+}
